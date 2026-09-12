b3d8027ad21ba140e50e0971bbf11b5bda90ac94
Fernando Méndez <20245099@esen.edu.sv>
2026-09-11 18:46:46 -0600

    Eliminar infra/ (reemplazada por modules/ y environments/)

--- infra/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "website_url" {
-  value = aws_s3_bucket_website_configuration.site.website_endpoint
-}
--- infra/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
-
-variable "bucket_name" {
-  description = "Nombre único global del bucket S3"
-  type        = string
-}
